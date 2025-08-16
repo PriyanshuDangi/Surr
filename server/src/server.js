9eb91b01f8fee17dc437bf479ec700c0a6bcdc04
Priyanshu Dangi <priyanshudangi37@gmail.com>
2025-08-16 00:05:03 -0400

    server structure and dependencies

---
+++ server/src/server.js
@@ -0,0 +1,7 @@
+// Surr Game - Server Entry Point
+// Main server file that initializes Express and Socket.IO
+
+console.log('Surr Game - Server Starting...');
+
+// Server implementation will be done in Phase 2
+// For now, this serves as the entry point placeholder
